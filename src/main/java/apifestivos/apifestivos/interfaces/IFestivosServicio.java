762668c768d454d05b4e493f181ce311155aac36
qxperez870 <qxperez870@gmail.com>
2023-12-04 17:29:16 -0500

    commit 1

--- src/main/java/apifestivos/apifestivos/interfaces/IFestivosServicio.java
+++ src/main/java/apifestivos/apifestivos/interfaces/IFestivosServicio.java
@@ -1,8 +1,10 @@
 package apifestivos.apifestivos.interfaces;
 
 import apifestivos.apifestivos.entidades.Festivo;
+import apifestivos.apifestivos.entidades.Tipo;
 
 import java.util.List;
+
 
 
 public interface IFestivosServicio {
@@ -10,5 +12,11 @@ public interface IFestivosServicio {
     public List<Festivo> listar();
 
     public String esFestivo(String date);
-    
+
+    public Festivo obtener(long id);
+
+    public Festivo guardar(Festivo festivo);
+
+    public boolean eliminar(long id);
+
 }
